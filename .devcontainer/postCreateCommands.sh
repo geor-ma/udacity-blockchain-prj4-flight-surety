2e49ef4ecbe3775d649eee11845ebbca253d7544
manishG <mg.work.livn@gmail.com>
2021-09-20 08:29:41 -0400

    initial setup

---
+++ .devcontainer/postCreateCommands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -x
+
+# https://stackoverflow.com/questions/48910876/error-eacces-permission-denied-access-usr-local-lib-node-modules
+
+mkdir ~/.npm-global
+
+npm config set prefix '~/.npm-global'
+
+echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
+
+source ~/.profile
+
+npm install -g truffle --unsafe-perm
+npm install -g ganache-cli --unsafe-perm
