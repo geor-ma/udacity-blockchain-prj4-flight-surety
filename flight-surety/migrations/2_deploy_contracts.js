8f46d75b29d9d6e0311fc53af4664472c1025917
manishG <mg.work.livn@gmail.com>
2021-09-22 14:00:50 +0000

    adding boilerplate code from template from udacity

--- flight-surety/migrations/2_deploy_contracts.js
+++ flight-surety/migrations/2_deploy_contracts.js
@@ -1,8 +1,35 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
+const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
+const fs = require("fs");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+
+  //flight surety
+  let firstAirline = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
+  deployer.deploy(FlightSuretyData).then(() => {
+    return deployer.deploy(FlightSuretyApp).then(() => {
+      let config = {
+        localhost: {
+          url: "http://localhost:8545",
+          dataAddress: FlightSuretyData.address,
+          appAddress: FlightSuretyApp.address,
+        },
+      };
+      fs.writeFileSync(
+        __dirname + "/../src/dapp/config.json",
+        JSON.stringify(config, null, "\t"),
+        "utf-8"
+      );
+      fs.writeFileSync(
+        __dirname + "/../src/server/config.json",
+        JSON.stringify(config, null, "\t"),
+        "utf-8"
+      );
+    });
+  });
 };
